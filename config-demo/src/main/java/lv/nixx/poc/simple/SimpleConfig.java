77997f720991ca73a6cb7163b4bc23e9039c664a
nixx78 <nixx78@gmail.com>
2017-02-21 21:13:39 +0200

    Minor refactoring done

---
+++ config-demo/src/main/java/lv/nixx/poc/simple/SimpleConfig.java
@@ -0,0 +1,10 @@
+package lv.nixx.poc.simple;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = SingleRunApplication.class)
+public class SimpleConfig {
+
+}
--- config-demo/src/main/java/lv/nixx/poc/simple/SingleRunApplication.java
+++ config-demo/src/main/java/lv/nixx/poc/simple/SingleRunApplication.java
@@ -1,17 +1,13 @@
 package lv.nixx.poc.simple;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackageClasses = SingleRunApplication.class)
 public class SingleRunApplication {
 
 	public static void main(String[] args) {
 
 		try (AnnotationConfigApplicationContext ctx = 
-				new AnnotationConfigApplicationContext(SingleRunApplication.class)) {
+				new AnnotationConfigApplicationContext(SimpleConfig.class)) {
 			
 			Engine e = ctx.getBean(Engine.class);
 			e.process();
