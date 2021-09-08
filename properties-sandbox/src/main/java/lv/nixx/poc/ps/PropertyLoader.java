946e49ce855d46cba00b327eab208f36fd5a7ef4
nixx78 <nixx78@gmail.com>
2021-09-08 16:52:43 +0300

    spring.config.import property added

--- properties-sandbox/src/main/java/lv/nixx/poc/ps/PropertyLoader.java
+++ properties-sandbox/src/main/java/lv/nixx/poc/ps/PropertyLoader.java
@@ -3,8 +3,11 @@ package lv.nixx.poc.ps;
 public class PropertyLoader {
 
     public static void loadProperties() {
-        System.setProperty("spring.config.additional-location",
-                "C:\\tmp\\configuration\\rest-sandbox\\external.properties");
+
+//        System.setProperty("spring.config.additional-location",
+//                "C:\\tmp\\configuration\\rest-sandbox\\external.properties");
+// также, можно использовать параметр: spring.config.import - он может находится в application.properties
+
         System.setProperty("spring.application.json", "{\"json.property\":\"json.property.value\"}");
     }
 }
