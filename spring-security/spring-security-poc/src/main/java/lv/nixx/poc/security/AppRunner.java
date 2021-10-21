5f387729656986ec5e69a27037cda7a97612077c
nixx78 <nixx78@gmail.com>
2021-10-21 12:04:38 +0300

    Tomcat server support added. Cookie playground added

--- spring-security/spring-security-poc/src/main/java/lv/nixx/poc/security/AppRunner.java
+++ spring-security/spring-security-poc/src/main/java/lv/nixx/poc/security/AppRunner.java
@@ -2,13 +2,18 @@ package lv.nixx.poc.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppRunner {
+public class AppRunner extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppRunner.class);
+	}
 
 	//TODO https://spring.io/guides/gs/securing-web/
-	// Add swagger
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppRunner.class, args);
 	}
--- spring-security/spring-security-poc/src/main/java/lv/nixx/poc/security/controller/SampleController.java
+++ spring-security/spring-security-poc/src/main/java/lv/nixx/poc/security/controller/SampleController.java
@@ -6,11 +6,17 @@ import lv.nixx.poc.security.service.SomeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @RestController
 public class SampleController {
@@ -27,9 +33,13 @@ public class SampleController {
     }
 
     @GetMapping("/secured")
-    public String getSample() {
-        LOG.info("/secured called");
-        return "Success:secured";
+    public ResponseEntity<String> getSample(@CookieValue(value = "count", required = false) Integer count , HttpServletResponse response) {
+        int newCount = count == null ? 1 : count + 1;
+
+        response.addCookie(new Cookie("count", String.valueOf(newCount)));
+        LOG.info("/secured called, execution count [{}]", newCount);
+
+        return new ResponseEntity<>("Success:secured", HttpStatus.OK);
     }
 
     @GetMapping("/basicSecured")
