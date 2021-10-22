5867e37ae02fdc1d8afb1b6e9cce5fd0e02a198d
nixx78 <nixx78@gmail.com>
2021-10-22 08:59:45 +0300

    Controller test fixed

--- spring-security/spring-security-poc/src/test/java/lv/nixx/poc/security/service/ControllerTest.java
+++ spring-security/spring-security-poc/src/test/java/lv/nixx/poc/security/service/ControllerTest.java
@@ -1,40 +1,39 @@
 package lv.nixx.poc.security.service;
 
-
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTest {
 
     @LocalServerPort
     private int localServerPort;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void unsecuredUrlTest() {
-        final ResponseEntity<String> res = new TestRestTemplate().getForEntity("http://localhost:" + localServerPort + "/home", String.class);
+        final ResponseEntity<String> res = restTemplate.getForEntity("http://localhost:" + localServerPort + "/account", String.class);
         assertEquals(HttpStatus.OK, res.getStatusCode());
-        assertEquals("Success:home", res.getBody());
+        assertEquals("AllAccounts", res.getBody());
     }
-
-    //https://stackoverflow.com/questions/15203485/spring-test-security-how-to-mock-authentication
 
     @Test
     public void securedUrlTest() {
 
-        final ResponseEntity<String> res = new TestRestTemplate()
-                .withBasicAuth("admin", "1")
-                .getForEntity("http://localhost:" + localServerPort + "/basicSecured", String.class);
+        final ResponseEntity<String> res = restTemplate.withBasicAuth("admin", "1")
+                .getForEntity("http://localhost:" + localServerPort + "/perform_login", String.class);
 
         assertEquals(HttpStatus.OK, res.getStatusCode());
-        assertEquals("Success:basicSecured", res.getBody());
     }
 
 
