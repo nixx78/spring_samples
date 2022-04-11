6894d7896cfc881e92b86c6380cacf995c8298f4
nixx78 <nixx78@gmail.com>
2022-04-11 14:56:12 +0300

    Code grooming & cleanup

--- SpringIOC_PoC/src/main/java/lv/nixx/poc/spring/ioc/ConfigUsageSample.java
+++ SpringIOC_PoC/src/main/java/lv/nixx/poc/spring/ioc/ConfigUsageSample.java
@@ -3,47 +3,22 @@ package lv.nixx.poc.spring.ioc;
 import org.springframework.context.annotation.*;
 
 @Configuration("appConfig")
-@ComponentScan(basePackages="lv.nixx.poc.spring.ioc")
+@ComponentScan(basePackages = "lv.nixx.poc.spring.ioc")
 public class ConfigUsageSample {
-	
-	@Bean
-	public Customer john(){
-		return new Customer("Jonh","Smith");
-	}
 
-	@Bean
-	public Customer mary(){
-		return new Customer("Mary","Luiza");
-	}
-	
-	@Bean
-	@Scope("prototype")
-	public Customer randomCustomer(){
-		return new Customer("n_" + System.currentTimeMillis(), "s_" + System.currentTimeMillis() );
-	}
-	
-	public static void main(String[] args) {
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigUsageSample.class);
-		
-		System.out.println(ctx.getBean("john"));
-		System.out.println(ctx.getBean("mary"));
-		
-		ConfigUsageSample bean = ctx.getBean("appConfig", ConfigUsageSample.class);
-		System.out.println(bean.john() + "!!!!!");
-		
-		ComplexBean complexBean = ctx.getBean("beanWithJohn", ComplexBean.class);
-		complexBean.sayHello();
-		
-		String[] names = ctx.getBeanNamesForType(Customer.class);
-		for (String n : names) {
-			System.out.println(n);
-		}
-		
-		Customer c1 = ctx.getBean("randomCustomer", Customer.class);
-		System.out.println(c1.hashCode() + ":" + c1);
+    @Bean
+    public Customer john() {
+        return new Customer("Jonh", "Smith");
+    }
 
-		Customer c2 = ctx.getBean("randomCustomer", Customer.class);
-		System.out.println(c2.hashCode() + ":" + c2);
-	}
-	
+    @Bean
+    public Customer mary() {
+        return new Customer("Mary", "Luiza");
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Customer randomCustomer() {
+        return new Customer("n_" + System.nanoTime(), "s_" + System.nanoTime());
+    }
 }
--- SpringIOC_PoC/src/main/java/lv/nixx/poc/spring/ioc/Customer.java
+++ SpringIOC_PoC/src/main/java/lv/nixx/poc/spring/ioc/Customer.java
@@ -1,34 +1,13 @@
 package lv.nixx.poc.spring.ioc;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = {"name", "surname"})
 public class Customer {
-
-	private String name;
-	private String surname;
-
-	public Customer(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [name=" + name + ", surname=" + surname + "]";
-	}
-
+    private final String name;
+    private final String surname;
 }
