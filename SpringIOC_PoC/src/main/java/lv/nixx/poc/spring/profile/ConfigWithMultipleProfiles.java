56f0be56195fbddbc8d79012ae2c6fc442e94109
nixx78 <nixx78@gmail.com>
2022-04-20 17:34:08 +0300

    Multiple profile sample added

---
+++ SpringIOC_PoC/src/main/java/lv/nixx/poc/spring/profile/ConfigWithMultipleProfiles.java
@@ -0,0 +1,47 @@
+package lv.nixx.poc.spring.profile;
+
+import lv.nixx.poc.spring.complex.Dto;
+import lv.nixx.poc.spring.complex.Source;
+import lv.nixx.poc.spring.complex.service.IService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ComponentScan(basePackages = "lv.nixx.poc.spring.profile")
+public class ConfigWithMultipleProfiles {
+
+    @Bean("pService")
+    @Profile("DEV")
+    public IService serviceForDev() {
+        return new IService() {
+            @Override
+            public Dto process(String request) {
+                return new Dto("Dev.Value." + request, "Dev.Value." + request);
+            }
+
+            @Override
+            public Source getSource() {
+                return null;
+            }
+        };
+    }
+
+    @Bean("pService")
+    @Profile("PROD")
+    public IService serviceForProd() {
+        return new IService() {
+            @Override
+            public Dto process(String request) {
+                return new Dto("Prod.Value." + request, "Prod.Value." + request);
+            }
+
+            @Override
+            public Source getSource() {
+                return null;
+            }
+        };
+    }
+
+}
---
+++ SpringIOC_PoC/src/test/java/lv/nixx/poc/spring/complex/ProdProfileTest.java
@@ -0,0 +1,32 @@
+package lv.nixx.poc.spring.complex;
+
+
+import lv.nixx.poc.spring.complex.service.IService;
+import lv.nixx.poc.spring.profile.ConfigWithMultipleProfiles;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ConfigWithMultipleProfiles.class)
+@ActiveProfiles("PROD")
+class ProdProfileTest {
+
+    @Autowired
+    @Qualifier("pService")
+    private IService service;
+
+    @Test
+    void processTest() {
+        Dto response = service.process("request");
+
+        assertEquals("Prod.Value.request", response.getField1());
+    }
+
+}
