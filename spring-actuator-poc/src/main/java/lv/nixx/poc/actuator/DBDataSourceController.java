dd067b33a45a863d853a8523b21bdcac585fd1ba
nixx78 <nixx78@gmail.com>
2024-10-15 10:24:34 +0300

    Minor changes in test controller

--- spring-actuator-poc/src/main/java/lv/nixx/poc/actuator/DBDataSourceController.java
+++ spring-actuator-poc/src/main/java/lv/nixx/poc/actuator/DBDataSourceController.java
@@ -1,5 +1,7 @@
 package lv.nixx.poc.actuator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,8 @@ import java.util.concurrent.TimeUnit;
 @RestController
 public class DBDataSourceController {
 
+    private static final Logger log = LoggerFactory.getLogger(DBDataSourceController.class);
+
     private final DataSource alphaDataSource;
 
     public DBDataSourceController(DataSource alphaDataSource) {
@@ -17,9 +21,15 @@ public class DBDataSourceController {
     }
 
     @GetMapping("/test/alpha")
-    public void testAlpha() throws Exception {
-        Connection connection = alphaDataSource.getConnection();
-        TimeUnit.SECONDS.sleep(60L);
-        connection.close();
+    public void testAlpha() {
+
+        try (Connection connection = alphaDataSource.getConnection()) {
+            log.info("Start connection check, connection: {}", connection.toString());
+            TimeUnit.SECONDS.sleep(60L);
+            log.info("Finish connection check");
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        }
     }
 }
